c9ac29e5e78ea4b42c951fe6b5b0d79ecb1d63a6
Giorgio Buzzanca <giorgiobuzzanca@outlook.it>
2022-01-19 18:47:05 +0100

    (scripts): add spacy support for pos and lemmatization

--- scripts/squad-reader.py
+++ scripts/squad-reader.py
@@ -2,6 +2,7 @@ import typing
 import json
 
 import click
+import spacy
 
 
 def load_raw_dataset(dataset_json: str) -> typing.Dict:
@@ -25,21 +26,54 @@ def load_raw_dataset(dataset_json: str) -> typing.Dict:
                 dataset['questions_ids'].append(question_answer['id'])
                 dataset['questions'].append(question_answer['question'])
                 dataset['contexts_ids'].append(current_context_id - 1)
-                if 'answers' in question_answer:
-                    dataset['answers'].append(question_answer['answers'])
+                dataset['answers'].append(question_answer['answers'])
 
     return dataset
 
 
+def preprocess(dataset):
+    output = []
+
+    nlp = spacy.load('en_core_web_trf')
+    contexts_docs = []
+    for context in dataset['context']:
+        contexts_docs.append(nlp(context))
+
+    for qidx, question in enumerate(dataset['questions']):
+        question_id = dataset['questions_ids'][qidx]
+        context_id = dataset['contexts_ids'][qidx]
+        question_doc = nlp(question)
+        context_doc = contexts_docs[context_id]
+        preprocessed_entry = {
+            'id':       question_id,
+            'question': [token.text for token in question_doc],
+            'question_pos': [token.pos_ for token in question_doc],
+            'question_lemma': [token.lemma_ for token in question_doc],
+            'context': [token.text for token in context_doc],
+            'context_pos': [token.pos_ for token in context_doc],
+            'context_lemma': [token.lemma_ for token in context_doc]
+            # 'answers': tbd
+        }
+
+        output.append(json.dumps(preprocessed_entry))
+
+    return output
+
+
+
 @click.command()
-@click.option("--squad-filepath", default="dataset/training_set.json", help="Filepath of the SQuAD dataset")
-@click.option("--output-filepath", default="dataset/preprocessed_training_set.txt",
-              help="Filepath of the preprocessed dataset")
+@click.option('--squad-filepath', default='dataset/training_set.json', help='Filepath of the SQuAD dataset')
+@click.option('--output-filepath', default='dataset/preprocessed_training_set.txt',
+              help='Filepath of the preprocessed dataset')
 def run(squad_filepath: str, output_filepath: str):
-    with open(squad_filepath, "r") as dataset_fd:
+    with open(squad_filepath, 'r') as dataset_fd:
         dataset_json = dataset_fd.read()
 
     dataset = load_raw_dataset(dataset_json)
+    preprocessed_entries = preprocess(dataset)
+
+    with open(output_filepath, "w") as output_file:
+        output_file.write("\n".join(preprocessed_entries))
 
 
 run()
