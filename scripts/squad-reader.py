555eb4666bfa6ec105567ff0ac5bedc07ebcff10
Giorgio Buzzanca <giorgiobuzzanca@outlook.it>
2022-01-27 17:42:42 +0100

    (scripts): added answer search in context

--- scripts/squad-reader.py
+++ scripts/squad-reader.py
@@ -9,11 +9,11 @@ def load_raw_dataset(dataset_json: str) -> typing.Dict:
     raw_data: typing.Dict = json.loads(dataset_json)['data']
 
     dataset = {
-        'questions_ids':    [],
-        'questions':        [],
-        'answers':          [],
-        'contexts':       [],
-        'contexts_ids':   []
+        'questions_ids': [],
+        'questions': [],
+        'answers': [],
+        'contexts': [],
+        'contexts_ids': []
     }
 
     current_context_id = 0
@@ -21,6 +21,7 @@ def load_raw_dataset(dataset_json: str) -> typing.Dict:
         paragraphs = document['paragraphs']
         for paragraph in paragraphs:
             dataset['contexts'].append(paragraph['context'])
+
             current_context_id += 1
 
             for question_answer in paragraph['qas']:
@@ -30,6 +31,20 @@ def load_raw_dataset(dataset_json: str) -> typing.Dict:
                 dataset['answers'].append(question_answer['answers'])
 
     return dataset
+
+
+def find_answer_in_context(offsets, start, end):
+    start_token = -1
+    end_token = -1
+    for idx, offset in enumerate(offsets):
+        if offset[0] == start:
+            start_token = idx
+        if offset[1] == end:
+            end_token = idx
+
+    assert start_token != -1 and end_token != -1, "Couldn't correctly find answer!"
+
+    return start_token, end_token
 
 
 def preprocess(dataset):
@@ -48,17 +63,20 @@ def preprocess(dataset):
     for qidx, question in enumerate(dataset['questions']):
         question_id = dataset['questions_ids'][qidx]
         context_id = dataset['contexts_ids'][qidx]
+        answer = dataset['answers'][qidx][0]  # only one answer per question
         question_doc = nlp(question)
         context_doc = contexts_docs[context_id]
+        context_offsets = [(token.idx, token.idx + len(token)) for token in context_doc]
         preprocessed_entry = {
-            'id':       question_id,
+            'id': question_id,
             'question': [token.text for token in question_doc],
             'question_pos': [token.pos_ for token in question_doc],
             'question_lemma': [token.lemma_ for token in question_doc],
             'context': [token.text for token in context_doc],
             'context_pos': [token.pos_ for token in context_doc],
-            'context_lemma': [token.lemma_ for token in context_doc]
-            # 'answers': tbd
+            'context_lemma': [token.lemma_ for token in context_doc],
+            'answer': find_answer_in_context(context_offsets, answer['answer_start'],
+                                             answer['answer_start'] + len(answer['text']))
         }
 
         output.append(json.dumps(preprocessed_entry))
