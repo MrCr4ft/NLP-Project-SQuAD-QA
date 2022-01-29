c41b6ada7d0f114de2fda3f8eac716ac60f286e2
Andrea Pinto <34470645+AndreaP097@users.noreply.github.com>
2022-01-29 12:36:08 +0100

    chore(.gitignore): Add models directory to gitignore  (#10)
    * (scripts): skeleton of the SQuAD reader
    
    * (scripts): raw dataset loading
    
    * (scripts): add type hints to reader
    
    * (scripts): add spacy support for pos and lemmatization
    
    * (scripts): fix some error in the reader and add progress status displaying
    
    * add models directory to gitignore
    
    Co-authored-by: Giorgio Buzzanca <giorgiobuzzanca@outlook.it>

---
+++ scripts/squad-reader.py
@@ -0,0 +1,87 @@
+import typing
+import json
+
+import click
+import spacy
+
+
+def load_raw_dataset(dataset_json: str) -> typing.Dict:
+    raw_data: typing.Dict = json.loads(dataset_json)['data']
+
+    dataset = {
+        'questions_ids':    [],
+        'questions':        [],
+        'answers':          [],
+        'contexts':       [],
+        'contexts_ids':   []
+    }
+
+    current_context_id = 0
+    for document in raw_data:
+        paragraphs = document['paragraphs']
+        for paragraph in paragraphs:
+            dataset['contexts'].append(paragraph['context'])
+            current_context_id += 1
+
+            for question_answer in paragraph['qas']:
+                dataset['questions_ids'].append(question_answer['id'])
+                dataset['questions'].append(question_answer['question'])
+                dataset['contexts_ids'].append(current_context_id - 1)
+                dataset['answers'].append(question_answer['answers'])
+
+    return dataset
+
+
+def preprocess(dataset):
+    output = []
+
+    nlp = spacy.load('en_core_web_trf')
+    contexts_docs = []
+    print("Processing contexts...")
+    for cidx, context in enumerate(dataset['contexts']):
+        contexts_docs.append(nlp(context))
+        percentage_completed = 100 * (cidx / len(dataset['contexts']))
+        if percentage_completed.is_integer():
+            print("%d%% processed" % percentage_completed)
+
+    print("Processing questions...")
+    for qidx, question in enumerate(dataset['questions']):
+        question_id = dataset['questions_ids'][qidx]
+        context_id = dataset['contexts_ids'][qidx]
+        question_doc = nlp(question)
+        context_doc = contexts_docs[context_id]
+        preprocessed_entry = {
+            'id':       question_id,
+            'question': [token.text for token in question_doc],
+            'question_pos': [token.pos_ for token in question_doc],
+            'question_lemma': [token.lemma_ for token in question_doc],
+            'context': [token.text for token in context_doc],
+            'context_pos': [token.pos_ for token in context_doc],
+            'context_lemma': [token.lemma_ for token in context_doc]
+            # 'answers': tbd
+        }
+
+        output.append(json.dumps(preprocessed_entry))
+        percentage_completed = 100 * (qidx / len(dataset['questions']))
+        if percentage_completed.is_integer():
+            print("%d%% processed" % percentage_completed)
+
+    return output
+
+
+@click.command()
+@click.option('--squad-filepath', default='dataset/training_set.json', help='Filepath of the SQuAD dataset')
+@click.option('--output-filepath', default='dataset/preprocessed_training_set.txt',
+              help='Filepath of the preprocessed dataset')
+def run(squad_filepath: str, output_filepath: str):
+    with open(squad_filepath, 'r') as dataset_fd:
+        dataset_json = dataset_fd.read()
+
+    dataset = load_raw_dataset(dataset_json)
+    preprocessed_entries = preprocess(dataset)
+
+    with open(output_filepath, "w") as output_file:
+        output_file.write("\n".join(preprocessed_entries))
+
+
+run()
