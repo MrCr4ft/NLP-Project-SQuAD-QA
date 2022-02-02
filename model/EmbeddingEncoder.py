dc8bcaf68cd780699868bf21f31629526f024ec1
AndreaP097 <pintoandrea097@gmail.com>
2022-02-02 17:27:06 +0100

    feat(layers): added Embedding Encoder layer

--- model/EmbeddingEncoder.py
+++ model/EmbeddingEncoder.py
@@ -1,21 +1,39 @@
-from turtle import forward
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 
-# TODO:
-# - positional encoder
-# - depthwise separable conv
-# - layer norm
-# - create 4 residual block with dw conv
-#
-# - self attention ...
-# - fc layer ...
+
+class Reshape1Dconv(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.conv1d = nn.Conv1d(
+            in_channels = self.in_channels,
+            out_channels = self.out_channels,
+            # This must be one otherwise the logic is different
+            kernel_size = 1
+        )
+
+    def forward(self, context, query):
+
+        context = context.permute(0, 2, 1)
+        context = self.conv1d(context)
+        context = context.permute(0, 2, 1)
+
+        query = query.permute(0, 2, 1)
+        query = self.conv1d(query)
+        query = query.permute(0, 2, 1)
+
+        return context, query
 
 
 class DepthwiseSeparableConv(nn.Module):
-    
-    def __init__(self, config):
+
+    def __init__(self, config: dict):
         super().__init__()
 
         self.in_channels = config['resized_emb_dim']
@@ -38,39 +56,90 @@ class DepthwiseSeparableConv(nn.Module):
         )
 
     def forward(self, x):
-        
+
+        x = x.permute(0, 2, 1)
         x = self.depthwise(x)
         x = self.pointwise(x)
+        x = x.permute(0, 2, 1)
+
+        return x
+
+
+class EncoderBlock(nn.Module):
+
+    # TODO: insert positional encoder in the fw pass
+
+    def __init__(self, config: dict):
+        super().__init__()
+
+        self.n_convs = config['enc_n_convs']
+        self.emb_dim = config['resized_emb_dim']
+
+        self.num_heads = config['self_att_num_heads']
+
+        self.conv = nn.ModuleList([DepthwiseSeparableConv(config) for _ in range(self.n_convs)])
+
+        self.conv_layer_norm = nn.ModuleList([nn.LayerNorm(self.emb_dim) for _ in range(self.n_convs)])
+        self.layer_norm1 = nn.LayerNorm(self.emb_dim)
+        self.layer_norm2 = nn.LayerNorm(self.emb_dim)
+
+        self.self_attention = nn.MultiheadAttention(
+            embed_dim = self.emb_dim,
+            num_heads = self.num_heads,
+            # dropout ?
+            batch_first = True
+        )
+
+        self.linear = nn.Linear(self.emb_dim, self.emb_dim)
+
+    def forward(self, x, attn_mask):
+
+        # Convolutional steps
+        for dw_convs, layer_norm in zip(self.conv, self.conv_layer_norm):
+            temp = layer_norm(x)
+            temp = F.relu(dw_convs(temp))
+            x = temp + x
+
+        # Self-attention
+        temp = self.layer_norm1(x)
+        # The embedding we want to project as query, key and value is the same
+        temp, _ = self.self_attention(
+            query = temp,
+            key = temp,
+            value = temp,
+            attn_mask = attn_mask
+        )
+        x = temp + x
+
+        # Linear layer
+        temp = self.layer_norm2(x)
+        temp = self.linear(x)
+        x = temp + x
 
         return x
 
 
 class EncoderEmbeddingLayer(nn.Module):
-    
-    def __init__(self, config):
+
+    def __init__(self, config: dict):
         super().__init__()
 
         self.in_channels = config['word_embed_dim'] + config['char_embed_out_dim']
         self.out_channels = config['resized_emb_dim']
+        self.num_enc_blocks = config['num_enc_blocks']
 
-        self.conv1d = nn.Conv1d(
-            in_channels = self.in_channels,
-            out_channels = self.out_channels,
-            # This must be one otherwise the logic is different
-            kernel_size=1
-        )
+        self.conv1d = Reshape1Dconv(self.in_channels, self.out_channels)
 
+        self.encoder_blocks = nn.ModuleList([EncoderBlock(config) for _ in range(self.num_enc_blocks)])
 
-    def forward(self, context, query):
+    def forward(self, context, query, C_attn_mask, Q_attn_mask):
 
         # 1D conv to reduce the embedding size
-        context = context.permute(0, 2, 1)
-        context = self.conv1d(context)
-        context = context.permute(0, 2, 1)
+        context, query = self.conv1d(context, query)
 
-        query = query.permute(0, 2, 1)
-        query = self.conv1d(query)
-        query = query.permute(0, 2, 1)
+        # Encoder embedding blocks pass
+        for block in self.encoder_blocks:
+            context = block(context, C_attn_mask)
+            query = block(query, Q_attn_mask)
 
-        # roba
-        return 
+        return context, query
