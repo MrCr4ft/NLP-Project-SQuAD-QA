ff430c50b75bd1be8a875a5c33d6fcfe8b9feb1b
AndreaP097 <pintoandrea097@gmail.com>
2022-02-04 18:17:21 +0100

    feat(layers): added ModelEncoder layer

---
+++ model/ModelEncoder.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from EmbeddingEncoder import Reshape1Dconv, EncoderBlock
+
+
+class ModelEncoder(nn.Module):
+
+    def __init__(self, config: dict):
+        super().__init__()
+
+        self.n_blocks = config['model_n_blocks']
+
+        self.emb_dim = config['resized_emb_dim']
+
+        self.reshape_conv = Reshape1Dconv(4*self.emb_dim, self.emb_dim)
+
+        self.model_blocks = nn.ModuleList(
+            [EncoderBlock(config, model = True) for _ in range(self.n_blocks)]
+        )
+
+    def forward(self, x, mask):
+        
+        """
+            The input x must have dimension (batch_size, #word_per_doc, 4*emb_dim)
+        """
+
+        # Reduce the dim of the embedding back to emb_dim
+        x = self.reshape_conv(x)
+
+        # Model encoder pass
+        # 'mask' should be the context mask with the same size, I'm not 100% sure about this
+        for block in self.model_blocks:
+            x = block(x, mask)
+        
+        return x
+        
