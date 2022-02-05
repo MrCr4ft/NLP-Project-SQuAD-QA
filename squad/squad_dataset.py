f1e29f2edea31df3c1e4ee15524b6f6975bde7bf
Giorgio Buzzanca <48859443+MrCr4ft@users.noreply.github.com>
2022-02-05 10:25:55 +0100

    feat(dataset): dataset loading and features extraction pipeline (#12)
    * src(utils): add __init__.py to utils
    
    * src(utils): first version of data reader
    
    * src(squad): init squad package
    
    * src(utils): complete features extraction' s pipeline implementation
    
    * src(squad): implement squad dataset class and collate function
    
    * src(utils): change load_glove_embeddings function to read also the 840B version
    
    * scripts(squad reader): complete refactoring
    
    * scripts(squad reader): added pos tagging handling and consecutive spaces replacement
    
    * src(utils): removed utils package after refactoring
    
    * scripts(squad reader): add an option to use the gpu at infer time when pos tagging is enabled
    
    * scripts(squad reader): remove contiguous spaces replacement
    
    Removed due to possible mismatches with the answers
    
    * scripts(squad reader): remove unused import
    
    * src(squad): refactor squad dataset class after changes in preprocessing
    
    * src(squad): add __len__ method to squad dataset
    
    * scripts(squad reader): save indexes of answers instead of one-hot encoding
    
    * feat(squad): reflect changes in preprocessing ops

---
+++ squad/squad_dataset.py
@@ -0,0 +1,46 @@
+import typing
+
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class SquadDataset(Dataset):
+
+    def __init__(self, dataset_filepath: str, include_pos: bool = False):
+        self.dataset = np.load(dataset_filepath)
+        self.include_pos = include_pos
+
+        self.contexts_widxs = torch.from_numpy(self.dataset['contexts_widxs']).long()
+        self.contexts_cidxs = torch.from_numpy(self.dataset['contexts_cidxs']).long()
+        self.questions_widxs = torch.from_numpy(self.dataset['questions_widxs']).long()
+        self.questions_cidxs = torch.from_numpy(self.dataset['questions_cidxs']).long()
+        self.answers_start_locations = torch.from_numpy(self.dataset['answers_start_locations']).long()
+        self.answers_end_locations = torch.from_numpy(self.dataset['answers_end_locations']).long()
+        self.qids = self.dataset['qids']
+        if self.include_pos:
+            self.contexts_pos = torch.from_numpy(self.dataset['contexts_widxs']).long()
+            self.questions_pos = torch.from_numpy(self.dataset['contexts_widxs']).long()
+
+        self.dataset_len = len(self.contexts_widxs)
+
+        super(SquadDataset, self).__init__()
+
+    def __getitem__(self, sample_idx: int) -> typing.Tuple:
+        item = (
+            self.contexts_widxs[sample_idx],
+            self.contexts_cidxs[sample_idx],
+            self.questions_widxs[sample_idx],
+            self.questions_cidxs[sample_idx],
+            self.answers_start_locations[sample_idx],
+            self.answers_end_locations[sample_idx],
+            self.qids[sample_idx]
+        )
+        if self.include_pos:
+            item += self.contexts_pos[sample_idx]
+            item += self.questions_pos[sample_idx]
+
+        return item
+
+    def __len__(self):
+        return self.dataset_len
